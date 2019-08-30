33e997c18ff10df87c30de335ce39a4b8497aac8
brettdoyle44 <brettdoyle44@gmail.com>
2019-08-30 09:39:57 -0400

    Update heroku link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://murmuring-stream-17692.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
